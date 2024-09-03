a009303fadb92d876d0538119a2581ed39b51711
manaim2112 <m.a.naim2112@gmail.com>
2024-09-03 11:50:43 +0700

    adding date explain

--- resources/js/Pages/AbsenReal.tsx
+++ resources/js/Pages/AbsenReal.tsx
@@ -5,12 +5,15 @@ import { useEffect, useState } from "react";
 export default function AbsenReal({ absen }: PageProps<{ absen: Absen[] }>) {
     const [a, setA] = useState<Absen[]|null>(null);
     useEffect(() => {
+        const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
+        const month = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "September", "Oktober", "November", "Desember"];
         const y = absen.map(r => {
             const createAt = new Date(String(r.created_at));
             const time = new Date(String(r.tanggal));
 
-            r.created_at = `${createAt.getDate()}/${createAt.getMonth()}/${createAt.getFullYear()}`;
-            r.tanggal = `${time.getDate()}/${time.getMonth()}/${time.getFullYear()}`;
+            r.created_at = `${createAt.getDate().toString().padStart(2, "0")}/${createAt.getMonth().toString().padStart(2, "0")}/${createAt.getFullYear()} ${createAt.getHours().toString().padStart(2, "0")}:${createAt.getMinutes().toString().padStart(2, "0")}:${createAt.getSeconds().toString().padStart(2, "0")}`;
+            
+            r.tanggal = `${days[time.getDay()]}, ${time.getDate().toString().padStart(2, "0")} ${month[time.getMonth()]} ${time.getFullYear()}`;
             return r;
         });
 
